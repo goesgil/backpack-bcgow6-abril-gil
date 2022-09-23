fe3bf31605ffd85d3d4b2af8ef8a8542a87db073
Maar GoesGil <abril.gil@mercadolibre.com>
2022-09-23 18:11:02 -0300

    feat: add ej3

--- goBasesTT/ejercicio3.go
+++ goBasesTT/ejercicio3.go
@@ -11,6 +11,22 @@ Ej: 7, Julio
 
 package main
 
-func main (){
-	
+import "fmt"
+
+func main() {
+	month := 3
+	var months = map[int]string{
+		1:  "Enero",
+		2:  "Febrero",
+		3:  "Marzo",
+		4:  "Abril",
+		5:  "Mayo",
+		6:  "Junio",
+		7:  "Julio",
+		8:  "Agosto",
+		9:  "Septiembre",
+		10: "Octubre",
+		11: "Noviembre",
+		12: "Diciembre"}
+	fmt.Println(months[month])
 }
