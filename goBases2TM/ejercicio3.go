774677ddbe209ade8af0c60b116dcb5dfeec1cc7
Maar GoesGil <abril.gil@mercadolibre.com>
2022-09-26 14:38:54 -0300

    feat: add ejercicio 3

--- goBases2TM/ejercicio3.go
+++ goBases2TM/ejercicio3.go
@@ -1,42 +1,61 @@
-Ejercicio 1 - Impuestos de salario
-Una empresa de chocolates necesita calcular el impuesto de sus empleados al momento de depositar el sueldo, para cumplir el objetivo es necesario crear una función que devuelva el impuesto de un salario. 
-Teniendo en cuenta que si la persona gana más de $50.000 se le descontará un 17% del sueldo y si gana más de $150.000 se le descontará además un 10%.
+/*
+￼Ejercicio 3 - calculate salary
+Una empresa marinera necesita calculate el salary de sus employees
+basándose en la cantidad de horas trabajadas By mes y la categoría.
 
+Si es categoría C, su salary es de $1.000 By hora
+Si es categoría B, su salary es de $1.500 By hora más un %20 de su salary mensual
+Si es de categoría A, su salary es de $3.000 By hora más un %50 de su salary mensual
 
-Ejercicio 2 - Calcular promedio
+Se solicita generar una función que reciba By parámetro la cantidad de
+minutes trabajados By mes y la categoría,
+y que devuelva su salary.
+*/
 
-Un colegio necesita calcular el promedio (por alumno) de sus calificaciones. Se solicita generar una función en la cual se le pueda pasar N cantidad de enteros y devuelva el promedio y un error en caso que uno de los números ingresados sea negativo
+package main
 
+import "errors"
+import "fmt"
 
+var (
+	CategoryA = "A"
+	CategoryB = "B"
+	CategoryC = "C"
+)
 
+func calculateSalaryByMinutes(minutes int, salary float64) float64 {
+	return float64(minutes) * salary / 60
+}
 
-￼Ejercicio 3 - Calcular salario
-Una empresa marinera necesita calcular el salario de sus empleados basándose en la cantidad de horas trabajadas por mes y la categoría.
+func salaryByEmployee(minutes int, category string) (float64, error) {
+	var (
+		salaryCategoryAByHora = 1000.00
+		salaryCategoryBByHora = 1500.00 + 1500.00*20/100
+		salaryCategoryCByHora = 3000.00 + 3000.00*50/100
+	)
+	if minutes < 0 {
+		return 0, errors.New("minutes must be greater than 0")
+	}
+	switch category {
+	case CategoryA:
+		return calculateSalaryByMinutes(minutes, salaryCategoryAByHora), nil
+	case CategoryB:
+		return calculateSalaryByMinutes(minutes, salaryCategoryBByHora), nil
+	case CategoryC:
+		return calculateSalaryByMinutes(minutes, salaryCategoryCByHora), nil
+	default:
+		return 0, errors.New("category not found")
+	}
+}
 
-Si es categoría C, su salario es de $1.000 por hora
-Si es categoría B, su salario es de $1.500 por hora más un %20 de su salario mensual
-Si es de categoría A, su salario es de $3.000 por hora más un %50 de su salario mensual
-
-Se solicita generar una función que reciba por parámetro la cantidad de minutos trabajados por mes y la categoría, y que devuelva su salario.
-
-
-
-Ejercicio 4 - Calcular estadísticas
-
-Los profesores de una universidad de Colombia necesitan calcular algunas estadísticas de calificaciones de los alumnos de un curso, requiriendo calcular los valores mínimo, máximo y promedio de sus calificaciones.
-
-Se solicita generar una función que indique qué tipo de cálculo se quiere realizar (mínimo, máximo o promedio) y que devuelva otra función ( y un mensaje en caso que el cálculo no esté definido) que se le puede pasar una cantidad N de enteros y devuelva el cálculo que se indicó en la función anterior
-
-
-Ejercicio 5 - Calcular cantidad de alimento
-
-Un refugio de animales necesita calcular cuánto alimento debe comprar para las mascotas. Por el momento solo tienen tarántulas, hamsters, perros, y gatos, pero se espera que puedan haber muchos más animales que refugiar.
-
-	1	perro necesitan 10 kg de alimento
-	2	gato 5 kg
-	3	Hamster 250 gramos.
-	4	Tarántula 150 gramos.
-
-Se solicita:
-	1	Implementar una función Animal que reciba como parámetro un valor de tipo texto con el animal especificado y que retorne una función y un mensaje (en caso que no exista el animal)
-	2	Una función para cada animal que calcule la cantidad de alimento en base a la cantidad del tipo de animal especificado.
+func main() {
+	// Try the next lines
+	// res, err := salaryByEmployee(9600, categoryB) // lun a vie 8hs = 9600 minutes
+	// res, err := salaryByEmployee(3940, "D")
+	res, err := salaryByEmployee(-8940, CategoryB)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+}
