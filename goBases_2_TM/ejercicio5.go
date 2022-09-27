85bec149a9a15f98b0e1bfb4b5810ae2b574cba6
Maar GoesGil <abril.gil@mercadolibre.com>
2022-09-27 11:31:06 -0300

    feat: add excercice 5

--- goBases_2_TM/ejercicio5.go
+++ goBases_2_TM/ejercicio5.go
@@ -1,32 +1,4 @@
-Ejercicio 1 - Impuestos de salario
-Una empresa de chocolates necesita calcular el impuesto de sus empleados al momento de depositar el sueldo, para cumplir el objetivo es necesario crear una función que devuelva el impuesto de un salario. 
-Teniendo en cuenta que si la persona gana más de $50.000 se le descontará un 17% del sueldo y si gana más de $150.000 se le descontará además un 10%.
-
-
-Ejercicio 2 - Calcular promedio
-
-Un colegio necesita calcular el promedio (por alumno) de sus calificaciones. Se solicita generar una función en la cual se le pueda pasar N cantidad de enteros y devuelva el promedio y un error en caso que uno de los números ingresados sea negativo
-
-
-
-
-￼Ejercicio 3 - Calcular salario
-Una empresa marinera necesita calcular el salario de sus empleados basándose en la cantidad de horas trabajadas por mes y la categoría.
-
-Si es categoría C, su salario es de $1.000 por hora
-Si es categoría B, su salario es de $1.500 por hora más un %20 de su salario mensual
-Si es de categoría A, su salario es de $3.000 por hora más un %50 de su salario mensual
-
-Se solicita generar una función que reciba por parámetro la cantidad de minutos trabajados por mes y la categoría, y que devuelva su salario.
-
-
-
-Ejercicio 4 - Calcular estadísticas
-
-Los profesores de una universidad de Colombia necesitan calcular algunas estadísticas de calificaciones de los alumnos de un curso, requiriendo calcular los valores mínimo, máximo y promedio de sus calificaciones.
-
-Se solicita generar una función que indique qué tipo de cálculo se quiere realizar (mínimo, máximo o promedio) y que devuelva otra función ( y un mensaje en caso que el cálculo no esté definido) que se le puede pasar una cantidad N de enteros y devuelva el cálculo que se indicó en la función anterior
-
+/*
 
 Ejercicio 5 - Calcular cantidad de alimento
 
@@ -40,3 +12,57 @@ Un refugio de animales necesita calcular cuánto alimento debe comprar para las 
 Se solicita:
 	1	Implementar una función Animal que reciba como parámetro un valor de tipo texto con el animal especificado y que retorne una función y un mensaje (en caso que no exista el animal)
 	2	Una función para cada animal que calcule la cantidad de alimento en base a la cantidad del tipo de animal especificado.
+
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	DogAnimal     = "DOG"
+	CatAnimal     = "CAT"
+	HamsterAnimal = "HAMSTER"
+	SpiderAnimal  = "SPIDER"
+)
+
+func calculateFoodBySpider(quantityAnimal float64) (quantityFood float64) {
+	return 0.150 * quantityAnimal
+}
+
+func calculateFoodByDog(quantityAnimal float64) (quantityFood float64) {
+	return 10000 * quantityAnimal
+}
+
+func calculateFoodByCat(quantityAnimal float64) (quantityFood float64) {
+	return 5000 * quantityAnimal
+}
+
+func calculateFoodByHamster(quantityAnimal float64) (quantityFood float64) {
+	return 0.250 * quantityAnimal
+}
+
+func FindAlimentByAnimal(typeAnimal string, quantityAnimal int) (quantityFood float64, err error) {
+	switch typeAnimal {
+	case SpiderAnimal:
+		return calculateFoodBySpider(float64(quantityAnimal)), nil
+	case DogAnimal:
+		return calculateFoodByDog(float64(quantityAnimal)), nil
+	case CatAnimal:
+		return calculateFoodByCat(float64(quantityAnimal)), nil
+	case HamsterAnimal:
+		return calculateFoodByHamster(float64(quantityAnimal)), nil
+	default:
+		return 0, errors.New("this animal not found")
+	}
+}
+func main() {
+	res, err := FindAlimentByAnimal(SpiderAnimal, 3)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("El animal %s, necesita %.3fgr de alimento\n", SpiderAnimal, res)
+}
