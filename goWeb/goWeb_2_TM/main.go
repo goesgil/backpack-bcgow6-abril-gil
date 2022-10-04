7d5315d3156bd966a81e2a0152037b6741ef8f51
Maar GoesGil <abril.gil@mercadolibre.com>
2022-10-04 13:41:05 -0300

    fix: update task TM

--- goWeb/goWeb_2_TM/main.go
+++ goWeb/goWeb_2_TM/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -12,16 +13,15 @@ var (
 
 type TransactionsRequest struct {
 	Id          int     `json:"id"`
-	CodeTrxs    string  `json:"codeTrxs" binding:"required" nullable:"false"`
-	Coin        string  `json:"coin" binding:"required" nullable:"false"`
-	Amount      float64 `json:"amount" binding:"required" nullable:"false"`
-	Transmitter string  `json:"transmitter" binding:"required" nullable:"false"`
+	CodeTrxs    string  `json:"codeTrxs" binding:"required"`
+	Coin        string  `json:"coin" binding:"required"`
+	Amount      float64 `json:"amount" binding:"required,min=0.00000000"`
+	Transmitter string  `json:"transmitter" binding:"required"`
 }
 
 func ValidateSession(c *gin.Context) {
 	if token := c.GetHeader("Authorization"); token == "" || strings.Split(token, " ")[1] != "123456" {
-		c.JSON(401, gin.H{"error": "no tiene permisos para realizar la petición solicitada"})
-		c.Abort()
+		c.AbortWithStatusJSON(401, gin.H{"error": "no tiene permisos para realizar la petición solicitada"})
 	}
 	c.Next()
 }
@@ -29,7 +29,7 @@ func ValidateSession(c *gin.Context) {
 func handleFunc(c *gin.Context) {
 	var req TransactionsRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.AbortWithStatusJSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	req.Id = len(transactions) + 1
@@ -39,7 +39,13 @@ func handleFunc(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	v1 := router.Group("/api/v1")
+	v1.Use(ValidateSession)
+	groupTrxs := v1.Group("/transactions")
+	groupTrxs.POST("/", ValidateSession, handleFunc)
+	groupTrxs.GET("/", func(c *gin.Context) {
+		c.JSON(200, transactions)
+	})
 
-	router.POST("/transactions", ValidateSession, handleFunc)
 	router.Run()
 }
